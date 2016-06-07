9dd3e4cdce6a31ab584a361eab6b2d8823318493
Kang HJ <kanghongjin@gmail.com>
2016-06-07 21:23:47 +0800

    fix indentation

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 var HttpClient = function() {
     this.get = function(aUrl, aCallback) {
-	anHttpRequest = new XMLHttpRequest();
-	anHttpRequest.onreadystatechange = function() { 
-	    if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-		aCallback(anHttpRequest.responseText);
-	}
-	anHttpRequest.open( 'GET', aUrl, true );						
-	anHttpRequest.send( null );
+    anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function() { 
+        if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
+    }
+    anHttpRequest.open( 'GET', aUrl, true );                        
+    anHttpRequest.send( null );
     }
 }
 
@@ -25,51 +25,51 @@ var translationUrl = '';
 
 function onWindowLoad() {
     chrome.storage.sync.get(null, function(result) {
-	    userAccount = result.userAccount;
-	    //console.log('user acc: '+ result.userAccount);
+        userAccount = result.userAccount;
+        //console.log('user acc: '+ result.userAccount);
 
-	    if (userAccount == undefined){
+        if (userAccount == undefined){
             var d = new Date();
             userAccount = 'id' + d.getTime() + '_1';
             chrome.storage.sync.set({'userAccount': userAccount}, function() {});
-	    }
-	    //console.log('userAccount ' + userAccount);
+        }
+        //console.log('userAccount ' + userAccount);
 
-	    isWorking = result.isWorking;
-	    if (isWorking == undefined) {
+        isWorking = result.isWorking;
+        if (isWorking == undefined) {
             isWorking = 1;
             chrome.storage.sync.set({'isWorking': isWorking});
-	    }
-	    //console.log('isWorking '+isWorking);
-	    if (isWorking == 0) {
+        }
+        //console.log('isWorking '+isWorking);
+        if (isWorking == 0) {
             document.getElementById('turnOn').className = 'btn btn-default';
             document.getElementById('turnOff').className = 'btn btn-primary active';
             
             $('.website-checkbox input').prop('disabled', true);
-	    } else {
+        } else {
             document.getElementById('turnOn').className = 'btn btn-primary active';
             document.getElementById('turnOff').className = 'btn btn-default';
             $('.website-checkbox input').prop('disabled', false);
-	    }
+        }
 
 
-	    wordDisplay = result.wordDisplay;
-	    if (wordDisplay == undefined) {
+        wordDisplay = result.wordDisplay;
+        if (wordDisplay == undefined) {
             wordDisplay = 1;
             chrome.storage.sync.set({'wordDisplay': wordDisplay});
-	    }
-	    console.log('wordDisplay '+wordDisplay);
-	    if (wordDisplay == 0) {
+        }
+        console.log('wordDisplay '+wordDisplay);
+        if (wordDisplay == 0) {
             document.getElementById('displayEnglish').className = 'btn btn-default';
             document.getElementById('displayChinese').className = 'btn btn-primary active';
-	    } else {
+        } else {
             document.getElementById('displayEnglish').className = 'btn btn-primary active';
             document.getElementById('displayChinese').className = 'btn btn-default';
-	    }
+        }
 
         translationUrl = result.translationUrl || "http://wordnews-mobile.herokuapp.com/";
         console.log('transUrl', translationUrl);
-	    if (translationUrl.indexOf('mobile') >= 0) {
+        if (translationUrl.indexOf('mobile') >= 0) {
             document.getElementById('dictionaryTranslations').className = 'btn btn-default';
             document.getElementById('imsTranslations').className = 'btn btn-default';
             document.getElementById('bingTranslations').className = 'btn btn-primary active';
@@ -79,24 +79,24 @@ function onWindowLoad() {
             document.getElementById('dictionaryTranslations').className = 'btn btn-primary active';
             document.getElementById('imsTranslations').className = 'btn btn-default';
             document.getElementById('bingTranslations').className = 'btn btn-default';
-	    } else {
+        } else {
             document.getElementById('dictionaryTranslations').className = 'btn btn-default';
             document.getElementById('imsTranslations').className = 'btn btn-primary active';
             document.getElementById('bingTranslations').className = 'btn btn-default';
-	    }
+        }
 
 
-	    wordsReplaced = result.wordsReplaced;
-	    //console.log('wordsReplaced '+wordsReplaced);
-	    if (wordsReplaced == undefined) {
+        wordsReplaced = result.wordsReplaced;
+        //console.log('wordsReplaced '+wordsReplaced);
+        if (wordsReplaced == undefined) {
             wordsReplaced = 2;
             //console.log('Set to default wordsReplaced setting');
             chrome.storage.sync.set({'wordsReplaced': wordsReplaced});
-	    } else {
-		//document.getElementById('wordsReplaced').value = wordsReplaced;
+        } else {
+        //document.getElementById('wordsReplaced').value = wordsReplaced;
             $('#wordsReplaced').slider({
-		    precision: 2,
-		    value: wordsReplaced // Slider will instantiate showing 8.12 due to specified precision
+            precision: 2,
+            value: wordsReplaced // Slider will instantiate showing 8.12 due to specified precision
             });
         }
 
@@ -140,8 +140,8 @@ function onWindowLoad() {
     });
 
     $('#wordsReplaced').on('slide', function(slideEvt) {
-	    chrome.storage.sync.set({'wordsReplaced': slideEvt.value});
-	});
+        chrome.storage.sync.set({'wordsReplaced': slideEvt.value});
+    });
 
 
     $('input').change(function() {
@@ -154,7 +154,7 @@ function onWindowLoad() {
         }
 
         if (document.getElementById('inlineCheckbox2').checked == true) {
-            if(websiteSetting !== '')	
+            if(websiteSetting !== '')   
                 websiteSetting += '_';
             websiteSetting += document.getElementById('inlineCheckbox2').value;
         }
@@ -220,7 +220,7 @@ function onWindowLoad() {
             });
 
             $(this).find('.btn').toggleClass('btn-default');
-            $(this).find('.btn').toggleClass('active');	
+            $(this).find('.btn').toggleClass('active'); 
 
             if ($(this).find('.btn-primary').size() > 0) {
                 $(this).find('.btn').toggleClass('btn-primary');
@@ -246,7 +246,7 @@ function onWindowLoad() {
             return;
         }
         $(this).find('.btn').toggleClass('btn-default');
-        $(this).find('.btn').toggleClass('active');	
+        $(this).find('.btn').toggleClass('active'); 
 
         if ($(this).attr('id') == 'englishchinese') {
             if (wordDisplay === 1) {
